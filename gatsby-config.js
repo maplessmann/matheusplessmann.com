df849e2ef38c177d8da2bd4c17af0337df086c48
Matheus Plessmann <maplessmann@gmail.com>
2020-07-04 14:17:35 -0300

    change site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Matheus Plessmann`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Matheus Plessmann`,
+      summary: `a front-end developer who loves to learn and share knowledge.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    description: `My personal blog.`,
+    siteUrl: `https://matheusplessmann.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `maplessmann`,
     },
   },
   plugins: [
