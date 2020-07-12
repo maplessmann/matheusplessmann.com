de8770eaf12df771de909fd9489144e5d52d88ad
Matheus Plessmann <maplessmann@gmail.com>
2020-07-12 13:30:43 -0300

    Create a custom theme for typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,7 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import customTheme from './typographyTheme'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
-
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(customTheme)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
@@ -19,5 +9,4 @@ if (process.env.NODE_ENV !== `production`) {
 }
 
 export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export const { rhythm, scale } = typography
---
+++ src/utils/typographyTheme.js
@@ -0,0 +1,22 @@
+const theme = {
+  baseFontSize: '16px',
+  baseLineHeight: '24px',
+  headerFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+  modularScales: [
+    {
+      scale: 2.25,
+    },
+    {
+      scale: 'octave',
+      maxWidth: '768px',
+    },
+  ],
+  headerGray: 20,
+  bodyGray: 20,
+  headerWeight: 600,
+  bodyWeight: 'normal',
+  boldWeight: 'bold',
+}
+
+export default theme
